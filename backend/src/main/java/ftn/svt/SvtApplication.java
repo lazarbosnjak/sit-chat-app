afa4e6ecd9b16272dc3c37a2c4ae88823d2e1deb
lazarbosnjak <lazar.bosnjak.ns@gmail.com>
2026-05-28 15:14:03 +0200

    init spring project

---
+++ backend/src/main/java/ftn/svt/SvtApplication.java
@@ -0,0 +1,13 @@
+package ftn.svt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ftn/svt/SvtApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.svt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
