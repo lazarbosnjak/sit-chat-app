264a1c1a1a8e386dcb8627609f3befae1ef2f4be
Lazar Bošnjak <lazar.bosnjak.ns@gmail.com>
2026-06-03 16:55:10 +0200

    feat: add Chat and Message Repository interfaces

---
+++ backend/src/main/java/ftn/svt/repository/ChatRepository.java
@@ -0,0 +1,11 @@
+package ftn.svt.repository;
+
+import ftn.svt.model.Chat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, UUID> {
+}
---
+++ backend/src/main/java/ftn/svt/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package ftn.svt.repository;
+
+import ftn.svt.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, UUID> {
+}
