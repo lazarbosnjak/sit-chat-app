c20f70217e71c47607fbb6c9c5d0a917d731e023
lazarbosnjak <lazar.bosnjak.ns@gmail.com>
2026-06-08 16:41:05 +0200

    feat(frontend): add registration form

---
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  templateUrl: './register.component.html',
+  imports: [ReactiveFormsModule, RouterLink],
+})
+export class RegisterComponent {
+  private http = inject(HttpClient);
+  private readonly urlRegexp = new RegExp(
+    /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/,
+  );
+  registerForm = new FormGroup(
+    {
+      username: new FormControl<string>('', [Validators.required]),
+      password: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+      repeatedPassword: new FormControl<string>('', [Validators.required, Validators.minLength(8)]),
+      email: new FormControl<string>('', [Validators.required, Validators.email]),
+      firstName: new FormControl<string>('', [Validators.required]),
+      lastName: new FormControl<string>('', [Validators.required]),
+      phoneNumber: new FormControl<string>(''),
+      pfpUrl: new FormControl<string>('', [Validators.pattern(this.urlRegexp)]),
+    },
+    {
+      validators: passwordsMatchValidator(),
+    },
+  );
+
+  handleSubmit() {
+    this.http
+      .post('http://localhost:8080/api/v0/auth/register', this.registerForm.value)
+      .subscribe({
+        next: () => {
+          alert('Success');
+        },
+        error: () => {
+          alert('Fail');
+        },
+      });
+  }
+
+  isInvalid(name: string): boolean {
+    const control = this.registerForm.get(name);
+    return !!control && control.touched && control.invalid;
+  }
+}
+
+function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const repeatedPassword = control.get('repeatedPassword')?.value;
+
+    return password === repeatedPassword ? null : { passwordMistmatch: true };
+  };
+}
