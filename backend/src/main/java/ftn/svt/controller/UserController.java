da4b83e79f9438aa25f643fd3484fcbda7432220
lazarbosnjak <lazar.bosnjak.ns@gmail.com>
2026-06-09 19:20:54 +0200

    feat(api): add /users/me

--- backend/src/main/java/ftn/svt/controller/UserController.java
+++ backend/src/main/java/ftn/svt/controller/UserController.java
@@ -5,7 +5,6 @@ import ftn.svt.model.dto.user.UserInfoDTO;
 import ftn.svt.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
 import java.util.UUID;
 
 @RestController
@@ -23,9 +21,27 @@ public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/me")
+    public ResponseEntity<?> getMe() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        assert auth != null;
+        User user = userService.findByUsername(auth.getName());
+        UserInfoDTO dto = new UserInfoDTO(
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhoneNumber(),
+                user.getEmail(),
+                user.getPfpUrl(),
+                user.getRole().toString(),
+                user.getCreatedAt(),
+                user.isEnabled()
+        );
+        return ResponseEntity.ok(dto);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getOneById(@PathVariable UUID id) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findOneById(id);
         UserInfoDTO dto = new UserInfoDTO(
                 user.getUsername(),
